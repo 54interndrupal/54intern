e9b1820f1ef44e197aa21e1a13a10c917c119ed4
Edison <edison_cyh@hotmail.com>
2013-02-22 14:11:42 +0800

    Committed by Edison

--- sites/all/themes/ffintern/templates/html.tpl.php
+++ sites/all/themes/ffintern/templates/html.tpl.php
@@ -45,6 +45,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
+  <meta property="wb:webmaster" content="ca44a64150e6fda0" />
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
