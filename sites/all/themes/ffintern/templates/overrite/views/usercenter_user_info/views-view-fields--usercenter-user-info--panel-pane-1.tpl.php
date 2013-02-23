794abef71e3cad1159ae5fedea76c2a31c06535f
Edison <edison_cyh@hotmail.com>
2013-02-23 16:55:32 +0800

    Committed by Edison

--- sites/all/themes/ffintern/templates/overrite/views/usercenter_user_info/views-view-fields--usercenter-user-info--panel-pane-1.tpl.php
+++ sites/all/themes/ffintern/templates/overrite/views/usercenter_user_info/views-view-fields--usercenter-user-info--panel-pane-1.tpl.php
@@ -33,13 +33,13 @@ drupal_add_js($js_file);
 
   <div class="views-field views-field-picture user-picture authcache-user-picture"><?php if(!$_authcache_is_cacheable)print $fields['picture']->content;?></div>
   <div class="views-field">
-    <a href="<?php print base_path() . 'user?qt-user_center_tab=2#qt-user_center_tab' ?>"><span style="font-size: 14px;color: #336600">申请的职位 ></span></a>
+    <a href="<?php print base_path() . 'user?qt-user_center_tab=2#qt-user_center_tab' ?>"><span style="font-size: 12px;color: #336600">申请的职位 ></span></a>
   </div>
   <div class="views-field">
-    <a href="<?php print base_path() . 'user?qt-user_center_tab=0#qt-user_center_tab' ?>"><span style="font-size: 14px;color: #000033">关注的企业 ></span></a>
+    <a href="<?php print base_path() . 'user?qt-user_center_tab=0#qt-user_center_tab' ?>"><span style="font-size: 12px;color: #000033">关注的企业 ></span></a>
   </div>
   <div class="views-field">
-    <a href="<?php print base_path() . 'user?qt-user_center_tab=3#qt-user_center_tab' ?>"><span style="font-size: 14px;color: #993300">收藏的文章 ></span></a>
+    <a href="<?php print base_path() . 'user?qt-user_center_tab=3#qt-user_center_tab' ?>"><span style="font-size: 12px;color: #993300">收藏的文章 ></span></a>
   </div>
   <div class="views-field views-field-edit">
     <div class="field-content">
