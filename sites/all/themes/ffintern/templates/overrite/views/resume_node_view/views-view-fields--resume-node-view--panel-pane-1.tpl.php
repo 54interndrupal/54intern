98648292ecdc4f2b053aace26839632191d61360
Edison <edison_cyh@hotmail.com>
2013-11-01 23:27:39 +0800

    Committed by Edison

--- sites/all/themes/ffintern/templates/overrite/views/resume_node_view/views-view-fields--resume-node-view--panel-pane-1.tpl.php
+++ sites/all/themes/ffintern/templates/overrite/views/resume_node_view/views-view-fields--resume-node-view--panel-pane-1.tpl.php
@@ -27,24 +27,24 @@
 
 <div>
   <div class="d-1">
-    <div class="views-field views-field-real-name"><?print $fields["field_real_name"]->content?></div>
+    <div class="views-field views-field-real-name"><?php print $fields["field_real_name"]->content?></div>
     <div class="d-2">
-      <div class="views-field views-field-picture"><?print $fields["picture"]->content?></div>
+      <div class="views-field views-field-picture"><?php print $fields["picture"]->content?></div>
       <div class="d-3">
-        <div class="views-field views-field-field-status"><?print $fields["field_status"]->content?></div>
+        <div class="views-field views-field-field-status"><?php print $fields["field_status"]->content?></div>
         |&nbsp;&nbsp;
-        <div class="views-field views-field-field-sex"><?print $fields["field_sex"]->content?></div>
+        <div class="views-field views-field-field-sex"><?php print $fields["field_sex"]->content?></div>
         |&nbsp;&nbsp;
-        <div class="views-field views-field-field-birth-date"><?print $fields["field_birth_date"]->content?></div>
+        <div class="views-field views-field-field-birth-date"><?php print $fields["field_birth_date"]->content?></div>
       </div>
-      <div class="views-field views-field-location"><label>居住地：</label><?print $fields["field_location"]->content?>
+      <div class="views-field views-field-location"><label>居住地：</label><?php print $fields["field_location"]->content?>
       </div>
-      <div class="views-field views-field-cell-phone"><label>电话：</label> <?printr ($fields["field_cell_phone"])?>
+      <div class="views-field views-field-cell-phone"><label>电话：</label> <?php print $fields["field_cell_phone"]->content?>
       </div>
       <div class="views-field views-field-email"><label>E-mail：</label>
 
         <div class="field-content"><a
-          href="mailto:<?print $fields['field_email']->content?>"><?print $fields["field_email"]->content?></a></div>
+          href="mailto:<?php print $fields['field_email']->content?>"><?php print $fields["field_email"]->content?></a></div>
       </div>
     </div>
   </div>
@@ -52,15 +52,15 @@
     <span class="s-1">教育情况</span>
 
     <div class="d-4-1">
-      <div class="views-field views-field-school"><label>所在学校：</label><?print $fields["field_school"]->content?> </div>
+      <div class="views-field views-field-school"><label>所在学校：</label><?php print $fields["field_school"]->content?> </div>
       <div class="views-field views-field-admission-date">
-        <label>入学时间：</label><?print $fields["field_admission_date"]->content?> </div>
+        <label>入学时间：</label><?php print $fields["field_admission_date"]->content?> </div>
     </div>
     <div class="d-4-2">
-      <div class="views-field views-field-disipline"><label>所在专业：</label><?print $fields["field_discipline"]->content?>
+      <div class="views-field views-field-disipline"><label>所在专业：</label><?php print $fields["field_discipline"]->content?>
       </div>
       <div class="views-field views-field-education">
-        <label>在读学历：</label><?print $fields["field_education"]->content?>
+        <label>在读学历：</label><?php print $fields["field_education"]->content?>
       </div>
     </div>
   </div>
@@ -68,14 +68,14 @@
 
 <div class="d-5">
   <span class="s-1">
-    求职信 ——  <?print $fields["field_letter_name"]->content?>
+    求职信 ——  <?php print $fields["field_letter_name"]->content?>
   </span>
-  <?print $fields["field_letter_body"]->content?>
+  <?php print $fields["field_letter_body"]->content?>
 </div>
 <div class="d-6">
   <span class="s-1">
-    简历附件 ——  <?print $fields["field_resume_name"]->content?>
+    简历附件 ——  <?php print $fields["field_resume_name"]->content?>
   </span>
-  <?print $fields["field_resume_attachement"]->content?>
-  <?print $fields["field_resume_memo"]->content?>
+  <?php print $fields["field_resume_attachement"]->content?>
+  <?php print $fields["field_resume_memo"]->content?>
 </div>
