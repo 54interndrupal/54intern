a9dd833489932ecb0a8938027a966c00bdfa78f0
Edison <edison_cyh@hotmail.com>
2013-11-20 07:26:49 +0800

    Committed by Edison

--- sites/all/themes/ffintern/templates/html.tpl.php
+++ sites/all/themes/ffintern/templates/html.tpl.php
@@ -63,16 +63,6 @@
 
 </head>
 <body class="<?php print $classes; ?> root" <?php print $attributes;?>>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-45881337-1', '54intern.com');
-  ga('send', 'pageview');
-
-</script>
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
