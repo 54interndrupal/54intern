db59a36d8185cfbebf5f3327aed8d5ae384bd0e8
Edison <edison_cyh@hotmail.com>
2013-11-22 15:25:56 +0800

    Committed by Edison

--- sites/all/themes/ffintern/templates/overrite/form/review-node-form.tpl.php
+++ sites/all/themes/ffintern/templates/overrite/form/review-node-form.tpl.php
@@ -7,18 +7,20 @@
         <h4 class="modal-title" id="reviewModalTitle">我要点评</h4>
       </div>
       <div class="modal-body">
-        <span class="review-tip">（请帮我们完善下面的评价，这会帮到更多圈友：）</span>
-        <?php print drupal_render($form["field_overall_value"])?>
-        <?php print drupal_render($form["field_treatment_value"])?>
-        <?php print drupal_render($form["field_training_value"])?>
-        <?php print drupal_render($form["field_regularize_value"])?>
+        <div class="review-modal-content">
+          <span class="review-tip">（请帮我们完善下面的评价，这会帮到更多圈友：）</span>
+          <?php print drupal_render($form["field_overall_value"])?>
+          <?php print drupal_render($form["field_treatment_value"])?>
+          <?php print drupal_render($form["field_training_value"])?>
+          <?php print drupal_render($form["field_regularize_value"])?>
 
-        <?php print drupal_render($form["body"])?>
-        <?php print drupal_render($form["field_company_evaluation"])?>
+          <?php print drupal_render($form["body"])?>
+          <?php print drupal_render($form["field_company_evaluation"])?>
 
 
-        <?php print drupal_render($form["actions"]);?>
-        <?php print drupal_render_children($form);?>
+          <?php print drupal_render($form["actions"]);?>
+          <?php print drupal_render_children($form);?>
+        </div>
       </div>
     </div>
   </div>
